bebd0d5f78f2dc4449f99f28f4485d4fad6d3b70
Imad <imad.el_guerch@edu.devinci.fr>
2021-03-05 23:36:08 +0100

    Exercice 3

--- Book.py
+++ Book.py
@@ -2,39 +2,137 @@
 
 from functools import total_ordering
 
-@total_ordering
 
-class Book :
-    def __init__(self ,name,):
+class Book() :
+    def __init__(self ,name):
         
-        list_buy
-        list_sell
+        self.name=name
+        self.list_buy=[]
+        self.list_sell=[]
+        self.cpt=0
+                       
+    def insert_buy(self,q , p):
+        self.cpt+=1
+        O=Order(p,q)
+        print("---Insert Buy",end=' ')
+        O.set_id(self.cpt)
+        print(repr(O) ," id= %d" % O.get_id()," on  %s"% self.name)
+        print("")
+        
+        if(self.list_sell!=[] and p>=self.list_sell[0].price  ):     
+            if(self.list_sell[0]==O):
+                print("Execute %s at %s on %s" % (q,self.list_sell[0].price,self.name))
+                del self.list_sell[0]      
+            else:
+                x=q
+                while(x>0):
+                    if(x<self.list_sell[0].quantity):
+                        self.list_sell[0].quantity-=x
+                        print("Execute %s at %s on %s" % (x,self.list_sell[0].price,self.name))
+                        x=0
+                    else:
+                        print("Execute %s at %s on %s" % (self.list_sell[0].quantity,self.list_sell[0].price,self.name))
+                        x-=self.list_sell[0].quantity
+                        
+                        del self.list_sell[0]
+        else:
+            self.list_buy.append(O)
+            self.list_buy=sorted(self.list_buy,reverse=True)
+        self.OrderBook()
         
     
-    def insert_Order(self, quantity, price, buy=True):
+    def insert_sell(self,q,p):
+        self.cpt+=1
+        O=Order(p,q)
+        print("---Insert Sell ",end=' ')
+        O.set_id(self.cpt)
+        print(repr(O) ," id= %d" % O.get_id()," on  %s"% self.name)
+        print("")
         
-    
+        if(self.list_buy!= [] and p<=self.list_buy[0].price):     
+            if(self.list_buy[0]==O):
+                print("Execute %s at %s on %s" % (q,self.list_buy[0].price,self.name))
+                del self.list_buy[0]      
+            else:
+                x=q
+                while(x>0):
+                    if(x<self.list_buy[0].quantity):
+                        self.list_buy[0].quantity-=x
+                        print("Execute %s at %s on %s" % (x,self.list_buy[0].price,self.name))
+                        x=0
+                    else:
+                        print("Execute %s at %s on %s" % (self.list_buy[0].quantity,self.list_buy[0].price,self.name))
+                        x-=self.list_buy[0].quantity
+                        
+                        del self.list_buy[0]                                            
+        
+        else:
+            self.list_sell.append(O)
+            self.list_sell=sorted(self.list_sell)
+            
+        self.OrderBook()   
+                
+       
+        
+    def ListBuy(self): 
+          for e in self.list_buy:
+              print(e)
+             
+    def ListSell(self): 
+          for e in self.list_sell[::-1]:
+              print(e)
+                
+    def OrderBook(self): 
+          print("Sell")
+          self.ListSell()
+          print("------------------------")
+          print("Buy")
+          self.ListBuy()
+          print("------------------------")
+          print(" ")
     
 
-        
+@total_ordering       
         
 class Order:
-    def __init__(self, quantity, price, buy=True):
-        self.__quantity = quantity
+    def __init__(self,price , quantity):
+        self.quantity = quantity
         self.price = price
-        self.buy = buy
-    def is_sell(self):
-        return not self.buy
-    
-    def quantity(self):
-        return self.__quantity if self.buy else -self.__quantity
-    
-    def __str__(self): # human-readable content
-        return "%s @ %s" % (self.quantity, self.price)
-    def __repr__(self): # unambiguous representation of the object
+        self.id=0
+        
+    def is_Buy(self):
+        return  self.buy
+  
+    def set_id(self,x):
+       self.id+=x
+    def get_id(self):
+       return self.id
+       
+    def __str__(self):
+        return "    %s @ %s  id= %d" % (self.quantity, self.price ,self.id)
+        
+    def __repr__(self): 
         return "Order(%s, %s)" % (self.quantity, self.price)
     
-    def __eq__(self, other): # self == other
+    def quantity(self):
+        return self.quantity if self.buy else -self.quantity
+    
+    def price(self):
+        return self.price 
+    
+    def __eq__(self, other): 
         return other and self.quantity == other.quantity and self.price == other.price
-    def __lt__(self, other): # self < other
+    def __lt__(self, other): 
         return other and self.price < other.price
+    
+    
+  
+
+
+        
+
+    
+
+
+
+
