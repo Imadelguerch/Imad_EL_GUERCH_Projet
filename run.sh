b64bedcda7225baf573b0711e8065e7001dfdb3b
Imad <imad.el_guerch@edu.devinci.fr>
2021-03-09 22:37:18 +0100

    please use . run.sh to launch the program

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-source .env/bin/activate
+python3 -m venv .env
+activate () {
+	.  ./.env/bin/activate
+}
+
+activate
 pip3 install -r requirements.txt
 pip freeze > requirements.txt
 python main.py
-
